3afbd8f5a0e143e0a8eda5fd7fff61ff652b3ead
Victor Barbosa Magalhães <vbarbosamagalhaes@carecontinuity.com>
2024-09-17 21:52:11 -0300

    Fix test

--- Simplify.ORM.Test/Builders/SimplifyCommandBuilderTest.cs
+++ Simplify.ORM.Test/Builders/SimplifyCommandBuilderTest.cs
@@ -1,24 +1,71 @@
-﻿using Simplify.ORM.Builders;
+﻿using Simplify.ORM.Attributes;
+using Simplify.ORM.Builders;
 using Simplify.ORM.Enumerations;
+using Simplify.ORM.Interfaces;
+using System.Data;
 using Xunit;
 
 namespace Simplify.ORM.Test.Builders
 {
+    [Table(NamingConvention.PascalCase)]
+    public partial class UserMockEntity : SimplifyEntity
+    {
+        public int UserMockId { get; set; }
+    }
+
+
     public class SimplifyCommandBuilderTest
     {
-        [Fact]
-        public void AddInsertTest()
+        public static IEnumerable<object[]> Types()
+        {
+            return [
+                [typeof(SimplifySQLServerCommandBuilder)],
+                [typeof(SimplifyPostgresSQLCommandBuilder)],
+                [typeof(SimplifyMySQLCommandBuilder)]
+            ];
+        }
+
+        private static ISimplifyCommandBuilder GetCommandInstance(Type type) => (ISimplifyCommandBuilder)Activator.CreateInstance(type)!;
+
+
+        [Theory]
+        [MemberData(nameof(Types))]
+        public void AddInsertTest(Type type)
         {
             var table = "User";
             var columnValues = new Dictionary<string, object>() {
                 { "Username", "Victor" },
                 { "Password", "secret" }
             };
-            var query = new SimplifyMySQLCommandBuilder()
-                .AddInsert(table, columnValues)
+
+            var queryBuilder = GetCommandInstance(type)
+                .AddInsert(table, columnValues);
+
+            var query = queryBuilder.BuildQuery();
+
+            string expectedQuery;
+            if (type == typeof(SimplifySQLServerCommandBuilder))
+                expectedQuery = "INSERT INTO [User] ([Username], [Password]) VALUES (@Username, @Password);";
+            else if (type == typeof(SimplifyPostgresSQLCommandBuilder))
+                expectedQuery = "INSERT INTO \"User\" (\"Username\", \"Password\") VALUES (@Username, @Password);";
+            else if (type == typeof(SimplifyMySQLCommandBuilder))
+                expectedQuery = "INSERT INTO `User` (`Username`, `Password`) VALUES (@Username, @Password);";
+            else
+                throw new NotSupportedException("Unsupported type");
+
+            Assert.Equal(expectedQuery, query);
+        }
+
+        [Fact]
+        public void AddInsertMockEntityTest()
+        {
+            var entity = new UserMockEntity { UserMockId = 10 };
+
+            var query = new SimplifyPostgresSQLCommandBuilder()
+                .AddInsert(entity)
                 .BuildQuery();
 
-            var expectedQuery = "INSERT INTO User (Username, Password) VALUES (@Username, @Password);";
+            var expectedQuery = "INSERT INTO UserMockEntity (UserMockId) VALUES (@UserMockId);";
 
             Assert.Equal(expectedQuery, query);
         }
